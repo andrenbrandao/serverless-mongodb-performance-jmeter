1cfed7db6affa7e1c8e69b681c1aaa7f8c8c7572
André Brandão <andrenbrandao@gmail.com>
2022-02-19 16:19:38 -0300

    Initial commit

---
+++ test/aws-cdk-ec2-mongodb-performance.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkEc2MongodbPerformance from '../lib/aws-cdk-ec2-mongodb-performance-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-ec2-mongodb-performance-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkEc2MongodbPerformance.AwsCdkEc2MongodbPerformanceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
