dccc33511d6a1435f408705cb675bf912d8b01b8
André Brandão <andrenbrandao@gmail.com>
2022-02-20 17:20:08 -0300

    add configuration for mongod process

--- lib/user-data.sh
+++ lib/user-data.sh
@@ -11,4 +11,21 @@ gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
 " > /etc/yum.repos.d/mongodb-org-5.0.repo
 
 yum install -y mongodb-org
+echo "storage:
+  dbPath: /var/lib/mongodb
+  journal:
+    enabled: true
+systemLog:
+  destination: file
+  logAppend: true
+  path: /var/log/mongodb/mongod.log
+net:
+  port: 27017
+  bindIp: 0.0.0.0
+replication:
+  replSetName: MongoReplicaSet
+processManagement:
+  timeZoneInfo: /usr/share/zoneinfo
+" > /etc/mongod.conf
+
 systemctl start mongod
