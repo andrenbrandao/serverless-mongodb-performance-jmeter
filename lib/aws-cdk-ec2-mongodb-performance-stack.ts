88ecfea694ac20849b4e8fcd1bea3cef331538ab
André Brandão <andrenbrandao@gmail.com>
2022-02-20 11:36:07 -0300

    create ec2 instance

--- lib/aws-cdk-ec2-mongodb-performance-stack.ts
+++ lib/aws-cdk-ec2-mongodb-performance-stack.ts
@@ -1,16 +1,46 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class AwsCdkEc2MongodbPerformanceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'mongodb-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 0,
+      subnetConfiguration: [
+        { name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC },
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkEc2MongodbPerformanceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const mongoSG = new ec2.SecurityGroup(this, 'mongoserver-sg', {
+      vpc,
+      allowAllOutbound: true,
+    });
+
+    mongoSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'allow SSH access from anywhere',
+    );
+
+
+    const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroup: mongoSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      keyName: 'aws-ec2-key-pair',
+    });
+
   }
 }
