dd14bfe14d2b621baa4d0501528fdacb03c9ffc0
André Brandão <andrenbrandao@gmail.com>
2023-03-10 23:19:48 -0300

    chore: create monorepo with serverless framework and aws cdk

---
+++ services/serverless.ts
@@ -0,0 +1,90 @@
+/* eslint-disable no-template-curly-in-string */
+import type { AWS } from '@serverless/typescript';
+
+import { hello } from './src/functions';
+
+const serverlessConfiguration: AWS = {
+  service: 'serverless-typescript',
+  frameworkVersion: '2',
+  useDotenv: true,
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+      packager: 'yarn',
+    },
+    stage: '${opt:stage, self:provider.stage}',
+    stages: ['dev', 'staging', 'production'],
+    prune: {
+      automatic: true,
+      number: 3,
+    },
+    alerts: {
+      dashboards: true,
+      definitions: {
+        '5XXErrors': {
+          name: '5XXErrors',
+          namespace: 'AWS/ApiGateway',
+          metric: '5XXError',
+          omitDefaultDimension: true,
+          dimensions: [
+            {
+              Name: 'ApiName',
+              Value: '${self:service}-${self:custom.stage}',
+            },
+            {
+              Name: 'Stage',
+              Value: '${self:custom.stage}',
+            },
+          ],
+          threshold: 5,
+          statistic: 'Sum',
+          period: 60,
+          evaluationPeriods: 1,
+          datapointsToAlarm: 1,
+          comparisonOperator: 'GreaterThanOrEqualToThreshold',
+        },
+      },
+      alarms: ['functionThrottles', 'functionErrors', '5XXErrors'],
+    },
+  },
+  plugins: [
+    'serverless-webpack',
+    'serverless-offline',
+    'serverless-stage-manager',
+    'serverless-prune-plugin',
+    'serverless-plugin-aws-alerts',
+  ],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: ['codedeploy:*'],
+        Resource: '*',
+      },
+    ],
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+      metrics: false, // activate to see CacheHits and Misses
+    },
+    logs: {
+      // activate to see API Gateway logs
+      restApi: {
+        accessLogging: false,
+        executionLogging: false,
+        level: 'INFO',
+        fullExecutionData: false,
+      },
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  functions: { hello },
+};
+
+module.exports = serverlessConfiguration;
