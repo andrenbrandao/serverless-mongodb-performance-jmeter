8bfdad3e7e9d729af87abe9396ce490102e49114
André Brandão <andrenbrandao@gmail.com>
2023-05-16 20:49:15 -0300

    feat(services): optimize getRegionById by not returning products in query

--- services/src/functions/getProductsByRegion/getRegionById.ts
+++ services/src/functions/getProductsByRegion/getRegionById.ts
@@ -2,7 +2,7 @@ import Region, { IRegion } from '@/models/Region';
 
 export async function getRegionById(regionId: string): Promise<IRegion | null> {
   try {
-    const region = await Region.findById(regionId);
+    const region = await Region.findById(regionId, { _id: 1, name: 1 });
     if (!region) {
       throw new Error('Region not found');
     }
